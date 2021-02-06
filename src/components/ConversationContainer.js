05bd9bb09e9d898f27b01363578e5825f300795a
github username <60378440+vmar13@users.noreply.github.com>
2021-02-06 17:33:40 -0500

    created appjs, search and conversationcontainer components

---
+++ src/components/ConversationContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ConversationContainer extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>LIST OF CONVERSATIONS GOES HERE</p>
+            </div>
+        )
+    }
+}
+
+export default ConversationContainer
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div>
+            <p>SEARCH BAR GOES HERE</p>
+        </div>
+        
+    )
+}
+
+export default Search 
